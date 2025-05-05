48c9f7988337fae30787ac5428aeaa9195668ca5
Kavindu Janojya <kjanojya@gmail.com>
2025-05-06 03:19:51 +0530

    Update some pages.
    Some of pages were deleted.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/SignInPage" element={<SignIn />} />
-        <Route path="/SignUpPage" element={<SignUp />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/ViewItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ViewItem = () => {
+  return (
+    <div>
+        <h1>View Item</h1>
+        <p>This is the view item page.</p>
+        <p>Here you can view the details of a specific item.</p>
+    </div>
+  )
+}
+
+export default ViewItem
